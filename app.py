dea0ade40d9b31f22866aa44cce9f3f8a77be00d
boo <eyurchenko@specialist.ru>
2023-01-22 21:00:02 +0700

    add migrations

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import random
 import sqlite3
 from pathlib import Path
@@ -12,22 +13,26 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{BASE_DIR / 'main.db'}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.app_context().push()
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 class QuoteModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     author = db.Column(db.String(32), unique=False)
     text = db.Column(db.String(255), unique=False)
+    rate = db.Column(db.Integer)
 
-    def __init__(self, author, text):
+    def __init__(self, author, text, rate=1):
         self.author = author
         self.text = text
+        self.rate = rate
 
     def to_dict(self):
         return {
             "id": self.id,
             "author": self.author,
-            "text": self.text
+            "text": self.text,
+            "rate": self.rate
         }
 
 
