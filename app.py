560f2bdf25475df87b615b973132b0620a57432a
boo <eyurchenko@specialist.ru>
2023-02-25 21:01:27 +0700

    final commit

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import random
 import sqlite3
 from pathlib import Path
 from flask import g
+from sqlalchemy.sql import func
 
 BASE_DIR = Path(__file__).parent
 app = Flask(__name__)
@@ -39,6 +40,7 @@ class QuoteModel(db.Model):
     author_id = db.Column(db.Integer, db.ForeignKey(AuthorModel.id))
     text = db.Column(db.String(255), unique=False)
     rate = db.Column(db.Integer, server_default="1", default="1", nullable=False)
+    created_date = db.Column(db.DateTime(timezone=True), server_default=func.now())
 
     def __init__(self, author, text, rate=1):
         self.author_id = author.id
@@ -51,6 +53,7 @@ class QuoteModel(db.Model):
             "text": self.text,
             "author": self.author.to_dict(),
             "rate": self.rate,
+            "created_date": self.created_date.strftime("%d.%m.%Y")
         }
 
 
